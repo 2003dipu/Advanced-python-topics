83137bd1e76ded5765ebe4eae76810df6f555f56
Dipu Singha <hunabopa@gmail.com>
2023-08-04 21:28:41 +0600

    Python GUI text area

---
+++ GUItextArea.py
@@ -0,0 +1,12 @@
+# text widget = functions like a text area, you can enter multiple lines of text
+from tkinter import *
+
+def submit():
+    input= text.get('1.0',END)
+    print(input)
+window = Tk()
+text = Text(window,bg='light yellow',font=('Times New Roman',25),height=9,width=20,padx=20,pady=20,fg='purple')
+text.pack()
+button = Button(window,command=submit, text='submit')
+button.pack()
+window.mainloop()
